9aea4b170f77e24914b36187d32a8af464a360e7
Yiyao Zhou <zhou0160@algonquinlive.com>
2019-10-09 22:31:30 -0400

    added getUser function and userCard component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import './App.css'
+import UserCard from './UserCard'
 
 class App extends React.Component{
 
@@ -10,15 +11,23 @@ class App extends React.Component{
 
   getUsers(){
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json)
+    .then(response => response.json())
     .then(users => this.setState({users}))
   }
 
+  componentDidMount(){
+    this.getUsers()
+  }
 
   render(){
+    const usersList = this.state.users.map(user => <UserCard key={user.id} user={user}/>)
+
     return (
       <div className="App">
         <Header/>
+        <section className="profile-list">
+          {usersList}
+        </section>
       </div>
     );
   }
---
+++ src/UserCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Avatar from './smile-regular.svg'
+import './UserCard.css'
+
+function profileCards(props){
+    return(
+        <div className="UserCard">
+            <div className="card-infor">
+            <div className="card-avatar">
+                <img src={Avatar} alt=""/>
+            </div>
+            <div className="card-body">
+                <p className="user-name">
+                    {props.user.name}<br/>
+                    <span className="user-email">{props.user.email}</span>
+                </p>
+                <p className="user-website">{props.user.website}</p>
+            </div>
+            </div>
+            <div className="card-action">
+                <button>Posts</button>
+                <button>Todo</button>
+            </div>
+        </div>
+    )
+}
+
+export default profileCards
