052bd1ffc1fa047d63a7b9d407bc227030fdfae3
Yiyao Zhou <zhou0160@algonquinlive.com>
2019-10-17 17:20:55 -0400

    Updates

---
+++ precache-manifest.098297c77019e816cb964c8771182bd6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39967c08dfb9b756c606ead2249afe08",
+    "url": "/mad9135-c1-react-router/index.html"
+  },
+  {
+    "revision": "7b5c4591963ebdaf93ab",
+    "url": "/mad9135-c1-react-router/static/css/main.87fa0a5f.chunk.css"
+  },
+  {
+    "revision": "574bf3f05a92469b2d50",
+    "url": "/mad9135-c1-react-router/static/js/2.720d5542.chunk.js"
+  },
+  {
+    "revision": "7b5c4591963ebdaf93ab",
+    "url": "/mad9135-c1-react-router/static/js/main.ccfe6ee2.chunk.js"
+  },
+  {
+    "revision": "5a33f4dd0fbb867db681",
+    "url": "/mad9135-c1-react-router/static/js/runtime-main.23e57686.js"
+  },
+  {
+    "revision": "1f301d23fe1719f50ac01ebb5d85cf6e",
+    "url": "/mad9135-c1-react-router/static/media/comments-regular.1f301d23.svg"
+  },
+  {
+    "revision": "1f44e83cbd55b5d942db4b7f4b956407",
+    "url": "/mad9135-c1-react-router/static/media/list-alt-regular.1f44e83c.svg"
+  },
+  {
+    "revision": "47b17d2261361fb193f6c5de7ffc4958",
+    "url": "/mad9135-c1-react-router/static/media/loading.47b17d22.svg"
+  },
+  {
+    "revision": "aca9471f38f6bd5c3455f179a87e00b9",
+    "url": "/mad9135-c1-react-router/static/media/smile-regular.aca9471f.svg"
+  }
+]);
